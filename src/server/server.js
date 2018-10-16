1ebf1b74728494e58c16d6b8c83a0f13e40686f4
Riley Griffin <rigriffin241@gmail.com>
2018-10-15 21:10:49 -0400

    change routes

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../dist`));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}/../dist/index.html`));
+    res.sendFile(path.join(`${__dirname}/../dist/index.html`));
 });
 
 app.listen(PORT, () => {
