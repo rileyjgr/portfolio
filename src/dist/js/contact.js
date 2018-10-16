d56b4088535787f7e9adef03667ee9dbe8626874
Riley Griffin <rigriffin241@gmail.com>
2018-10-16 19:19:01 -0400

    make it better

---
+++ src/dist/js/contact.js
@@ -0,0 +1,35 @@
+// sign up form logic
+"use strict";
+
+const submit = document.getElementById("submit");
+
+submit.onclick = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    // const confirmEmail = document.getElementById("email-confirm").value;
+    const message = document.getElementById("message").value;
+    // const confirmPassword = document.getElementById("password-confirm").value;
+    // noinspection JSAnnotator
+    const data = {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    };
+
+    const sendMessage = () => {
+        const url = "/contact";
+        axios
+            .post(url, data)
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    };
+
+    console.log(data);
+    sendMessage();
+};
