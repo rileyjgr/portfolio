0ed808963c90265029cc9a55f2b5573de7b30539
Riley Griffin <rigriffin241@gmail.com>
2018-11-21 18:00:02 -0500

    change readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # portfolio
+
+This is a basic MERN app.
+
+# Features
+
+Automous updates on projects page. Code to not have to code anymore ? :thinking:
--- src/client/components/ProjectsPage.jsx
+++ src/client/components/ProjectsPage.jsx
@@ -42,31 +42,6 @@ class ProjectsPage extends Component {
                 <div id="projects">
                     {this.state.projects.map(project => <div><p>-----</p><li>{project.name}</li><li>{project.desc}</li><a href={project.url}>View on Github</a><p>-----</p></div>)}
                 </div>
-                {/* <div id="Animator">
-                    <h1>{this.state.projects.animator}</h1>
-                    <h3>{this.state.projects.animator.desc}</h3>
-                    <a href={this.state.projects.animator.url}>View on Github</a>
-                </div>
-                <div id="ImpactApi">
-                    <h1>{this.state.projects.astapi.name}</h1>
-                    <h3>{this.state.projects.astapi.desc}</h3>
-                    <a href={this.state.projects.astapi.url}>View on Github</a>
-                </div>
-                <div id="basic-gif-page">
-                    <h1>{this.state.projects.basic-gif-page.name}</h1>
-                    <h3>{this.state.projects.basic-gif-page.desc}</h3>
-                    <a href={this.state.projects.basic-gif-page.url}>View on Github</a>
-                </div>
-                <div id="basic-rpg">
-                    <h1>{this.state.projects.basic-rpg.name}</h1>
-                    <h3>{this.state.projects.basic-rpg.desc}</h3>
-                    <a href={this.state.projects.basic-rpg.url}>View on Github</a>
-                </div>
-                <div id="clickygame">
-                    <h1>{this.state.projects.clickygame.name}</h1>
-                    <h3>{this.state.projects.clickygame.desc}</h3>
-                    <a href={this.state.projects.clickygame.url}>View on Github</a>
-                </div>  */}
             </div>
         )
     }
