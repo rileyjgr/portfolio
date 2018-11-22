aa6a238b13426c6db1d80a2d9f27fee88b37c3d8
Riley Griffin <rigriffin241@gmail.com>
2018-11-22 13:27:26 -0500

    typo fix

--- src/client/components/ContactPage.jsx
+++ src/client/components/ContactPage.jsx
@@ -14,7 +14,7 @@ class ContactPage extends Component{
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-lg-5 col-md-8">
-                            <p>Email: <a href="#">rigrffin241@gmail.com</a></p>
+                            <p>Email: <a href="#">rigriffin241@gmail.com</a></p>
                             <p>Mobile: <a href="#">(908)-578-7632</a></p>
                         </div>
                     </div>
